8dee5d133b82da31646936d711793fe99d1c99eb
Vincent Howard <vincenguyenhoward@gmail.com>
2024-01-16 16:17:21 -0700

    refactored structure

---
+++ src/App.ts
@@ -0,0 +1,40 @@
+import './styles/output.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
+
+class App {
+  constructor(private rootElement: HTMLElement) {}
+
+  init() {
+    this.render();
+    setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+  }
+
+  render() {
+    const html = String.raw;
+    this.rootElement.innerHTML = html`
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="${viteLogo}" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://www.typescriptlang.org/" target="_blank">
+          <img
+            src="${typescriptLogo}"
+            class="logo vanilla"
+            alt="TypeScript logo"
+          />
+        </a>
+        <h1>Vite + TypeScript</h1>
+        <div class="card">
+          <button id="counter" type="button"></button>
+        </div>
+        <p class="read-the-docs font-bold">
+          Click on the Vite and TypeScript logos to learn more
+        </p>
+      </div>
+    `;
+  }
+}
+
+export default App;
