f4296e467fd9ab7b29d4081f48fe0284fa7a5564
Vincent Howard <vincenguyenhoward@gmail.com>
2024-01-16 17:01:05 -0700

    boilerplate threejs

--- src/App.ts
+++ src/App.ts
@@ -1,37 +1,20 @@
 import './styles/output.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+// @ts-ignore
+import Experience from './three/experience/Experience';
+import { html } from './utils/html.ts';
 
 class App {
   constructor(private rootElement: HTMLElement) {}
 
   init() {
     this.render();
-    setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+    const experience = new Experience(document.querySelector('canvas.webgl'));
   }
 
   render() {
-    const html = String.raw;
     this.rootElement.innerHTML = html`
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="${viteLogo}" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.typescriptlang.org/" target="_blank">
-          <img
-            src="${typescriptLogo}"
-            class="logo vanilla"
-            alt="TypeScript logo"
-          />
-        </a>
-        <h1>Vite + TypeScript</h1>
-        <div class="card">
-          <button id="counter" type="button"></button>
-        </div>
-        <p class="read-the-docs font-bold">
-          Click on the Vite and TypeScript logos to learn more
-        </p>
+        <canvas class="webgl"></canvas>
       </div>
     `;
   }
