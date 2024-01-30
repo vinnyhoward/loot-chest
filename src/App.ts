9197cd5464a86bd762a3a7473e75fc2816e4aa7d
Vincent Howard <vincenguyenhoward@gmail.com>
2024-01-29 23:13:32 -0700

    testing backend

--- src/App.ts
+++ src/App.ts
@@ -13,6 +13,30 @@ class App {
     this.loadingBar = new LoadingBar();
     this.render();
     new Experience(document.querySelector('canvas.webgl'));
+    this.fetchChests();
+  }
+
+  async fetchChests() {
+    const token: string =
+      'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJjbHJ2aHg4YWswMDAwMTBud2JidTFmaW1wIn0.LDKPeG6r7ElF42HF_ogFvIINado6VNcjz4ZQlOA5ais';
+    try {
+      // Assuming you have a setup to replace `process.env.API_URL` correctly
+      const response = await fetch(`${process.env.API_URL}/chests/all`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log('data', data);
+      // Do something with the data here, like updating the state or the UI
+    } catch (error) {
+      console.error('Failed to fetch chests:', error);
+      // Handle the error appropriately in your application
+    }
   }
 
   render() {
