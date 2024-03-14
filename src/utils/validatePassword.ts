e59bd757a8c6d1067c4da2c9b3e6a5c491197d5a
Vincent Howard <vincenguyenhoward@gmail.com>
2024-03-14 15:55:25 -0600

    lint fix

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -1,4 +1,4 @@
-export const validatePassword = (password: string): boolean => {
+export const validatePassword = (): boolean => {
   // const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
   // return re.test(password);
   return true;
